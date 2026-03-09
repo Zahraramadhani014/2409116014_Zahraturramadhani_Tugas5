4b2eb782455f37cc5d7e086ff2ff98df6b9f3b3c
Zahraturramadhani <zahraramadhani709@gmail.com>
2026-03-09 10:25:13 +0800

    tugas2_pab

---
+++ README.md
@@ -0,0 +1,3 @@
+# tugas2_pab
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/registration_provider.dart';
+import 'pages/registration_page.dart';
+import 'pages/registrant_list_page.dart';
+import 'pages/registrant_detail_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => RegistrationProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Registrasi Event Kampus',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.pinkAccent,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const RegistrationPage(),
+        '/list': (context) => const RegistrantListPage(),
+        '/detail': (context) => const RegistrantDetailPage(),
+      },
+    );
+  }
+}
